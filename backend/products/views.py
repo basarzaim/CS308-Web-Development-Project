ef46fa0644b84b737bf2ec2e7917208189ee100c
efekumru <efe.kumru@sabanciuniv.edu>
2025-11-20 20:29:16 +0300

    CT6-22: Implement product search & sorting with DRF filters

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductListView(generics.ListAPIView):
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-# Create your views here.
+    # Search & Sort aktif
+    filter_backends = [SearchFilter, OrderingFilter]
+
+    # /api/products/products/?search=mac
+    search_fields = ["name", "description"]
+
+    # /api/products/products/?ordering=price  OR  -price
+    ordering_fields = ["price", "name"]
+
