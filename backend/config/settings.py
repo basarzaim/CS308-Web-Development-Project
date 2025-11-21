f98787c697efa4c7f5d41d7d3a629c7b3df72d86
Riwex <lordark356@gmail.com>
2025-11-21 16:28:19 +0300

    Switch to SGLite for local dev.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,21 +117,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.postgresql",
+#        "NAME": os.getenv("DB_NAME"),
+#        "USER": os.getenv("DB_USER"),
+#        "PASSWORD": os.getenv("DB_PASSWORD"),
+#        "HOST": os.getenv("DB_HOST"),
+#        "PORT": os.getenv("DB_PORT"),
+#        "OPTIONS": {
+#            "sslmode": os.getenv("DB_SSL", "require"),  
+##        },
+#    }
+#}
+#
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "OPTIONS": {
-            "sslmode": os.getenv("DB_SSL", "require"),  
-        },
-    }
-}
-
-
+       "default": {
+           "ENGINE": "django.db.backends.sqlite3",
+           "NAME": BASE_DIR / "db.sqlite3",
+       }
+   }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
