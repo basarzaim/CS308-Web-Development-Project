6f1bd20b3e44a31800f7844377127e56373857d3
Basar Zaim <basarzaim@outlook.com>
2025-12-05 15:15:22 +0300

    postgre confusion fixed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -137,10 +137,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #}
 #
 DATABASES = {
-       "default": {
-           "ENGINE": "django.db.backends.sqlite3",
-           "NAME": BASE_DIR / "db.sqlite3",
-       }
+       
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+            "OPTIONS": {
+                "sslmode": os.getenv("DB_SSL", "require"),  
+            },
+        }
    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
